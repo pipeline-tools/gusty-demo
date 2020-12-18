dacc8b9855ec8a291c4fb5e81c406226af7be731
chriscardillo <cfcardillo23@gmail.com>
2020-12-18 07:53:48 -0500

    removing legacy demo

--- airflow/dags/tutorial_example.py
+++
@@ -1,8 +0,0 @@
-# This DAG lines up one-to-one with the Airflow tutorial:
-# https://airflow.apache.org/docs/stable/tutorial.html
-
-import os
-from gusty import GustyDAG
-
-dag_directory = os.path.splitext(os.path.abspath(__file__))[0]
-dag = GustyDAG(dag_directory)
