3cb72e682c0174bb48aa2845788eae4cf5ee052c
chriscardillo <cfcardillo23@gmail.com>
2023-03-22 18:24:44 -0400

    update gusty-demo

--- airflow/demo-setup/datawarehouse_setup.py
+++ airflow/demo-setup/datawarehouse_setup.py
@@ -3,16 +3,18 @@ from sqlalchemy_utils import create_database, database_exists
 import sqlalchemy
 
 uri = PostgresHook.get_connection('postgres_default').get_uri()
-engine = sqlalchemy.create_engine(uri)
+engine = PostgresHook().get_sqlalchemy_engine()
 
 # create database
 if not database_exists(engine.url):
     create_database(engine.url)
-    engine.execute("GRANT ALL PRIVILEGES ON DATABASE {db} TO {user};".format(user = engine.url.username, db = engine.url.database))
+    with engine.begin() as conn:
+        conn.execute("GRANT ALL PRIVILEGES ON DATABASE {db} TO {user};".format(user = engine.url.username, db = engine.url.database))
 
 # create schema, give permissions
 if not engine.dialect.has_schema(engine, 'views'):
-    engine.execute(sqlalchemy.schema.CreateSchema('views'))
-    engine.execute("GRANT ALL PRIVILEGES ON SCHEMA views TO {user};".format(user = engine.url.username))
-    engine.execute("GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA views TO {user};".format(user = engine.url.username))
-    engine.execute("ALTER DEFAULT PRIVILEGES IN SCHEMA views GRANT ALL PRIVILEGES ON TABLES TO {user};".format(user = engine.url.username))
+    with engine.begin() as conn:
+        conn.execute(sqlalchemy.schema.CreateSchema('views'))
+        conn.execute("GRANT ALL PRIVILEGES ON SCHEMA views TO {user};".format(user = engine.url.username))
+        conn.execute("GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA views TO {user};".format(user = engine.url.username))
+        conn.execute("ALTER DEFAULT PRIVILEGES IN SCHEMA views GRANT ALL PRIVILEGES ON TABLES TO {user};".format(user = engine.url.username))
